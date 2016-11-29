9e6d7ce70a573612a3c7b356d484c16bfeb01465
Fernão Lopes <fernao@riseup.net>
2016-11-29 18:24:28 -0200

    [ctav] alterações nos formulários para gerar inscrições sequenciais

--- functions.php
+++ functions.php
@@ -4,4 +4,29 @@ function divi_child_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+// Add Shortcode
+function num_inscricao() {
+  $num_inscricao = get_option('ctav_inscricoes', 500);
+  return $num_inscricao;
+}
+
+add_shortcode( 'num_inscricao', 'num_inscricao');
+
+function set_nova_inscricao() {
+  $num_inscricao = get_option('ctav_inscricoes') + 1;
+  update_option('ctav_inscricoes', $num_inscricao);
+}
+
+// funcao para executar antes do envio de emails
+function gera_nova_inscricao($cf7) {
+    set_nova_inscricao();
+    $num_inscricao = num_inscricao();
+    
+    $wpcf = WPCF7_ContactForm::get_current();
+    $wpcf->posted_data['numero_inscricao'] = $num_inscricao;
+    return $wpcf;
+}
+add_action('wpcf7_before_send_mail', 'gera_nova_inscricao');
+
 ?>

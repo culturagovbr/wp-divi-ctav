297c82227b0b50be2038f2b34277dd0cfcc0504a
Fernão Lopes <fernao@riseup.net>
2016-12-07 16:24:17 -0200

    [wp-divi-ctav] - funcionalidade de formulário

--- functions.php
+++ functions.php
@@ -5,28 +5,37 @@ function divi_child_enqueue_styles() {
 
 }
 
-// Add Shortcode
-function num_inscricao() {
+/*
+ * formulário de contato - funções para pegar número de inscricao
+ */
+
+// retorna numero de inscricao
+function get_num_inscricao() {
   $num_inscricao = get_option('ctav_inscricoes', 500);
   return $num_inscricao;
 }
 
-add_shortcode( 'num_inscricao', 'num_inscricao');
-
+// define nova inscricao
 function set_nova_inscricao() {
   $num_inscricao = get_option('ctav_inscricoes') + 1;
   update_option('ctav_inscricoes', $num_inscricao);
 }
 
 // funcao para executar antes do envio de emails
-function gera_nova_inscricao($cf7) {
+function numero_nova_inscricao($posted_data) {
+    $nova_inscricao = get_num_inscricao();
+    $posted_data['numero_inscricao'] = $nova_inscricao;
+    return $posted_data;
+}
+
+// incrementa inscricao no hook de email pronto para envio (pos-validacao)
+function incrementa_inscricao($wpcf) {
     set_nova_inscricao();
-    $num_inscricao = num_inscricao();
-    
-    $wpcf = WPCF7_ContactForm::get_current();
-    $wpcf->posted_data['numero_inscricao'] = $num_inscricao;
     return $wpcf;
 }
-add_action('wpcf7_before_send_mail', 'gera_nova_inscricao');
+
+add_shortcode( 'num_inscricao', 'num_inscricao');
+add_action('wpcf7_before_send_mail', 'set_nova_inscricao');
+add_action('wpcf7_posted_data', 'numero_nova_inscricao');
 
 ?>

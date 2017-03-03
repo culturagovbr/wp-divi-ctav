add64c7a7f3f0bf24221ef0ac28a996a69be3117
Fernão Lopes <fernao@riseup.net>
2017-03-03 16:49:14 -0300

    adicionando esquema novo de css do divi; adicionando arquivos de alto contraste

--- functions.php
+++ functions.php
@@ -38,4 +38,12 @@ add_shortcode( 'num_inscricao', 'num_inscricao');
 add_action('wpcf7_before_send_mail', 'set_nova_inscricao');
 add_action('wpcf7_posted_data', 'numero_nova_inscricao');
 
+function divi_child_enqueue_styles() {
+    wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/custom.css');
+    wp_enqueue_style( 'alto-contraste', get_stylesheet_directory_uri() . '/alto-contraste.css');
+    wp_enqueue_script( 'alto-contraste', get_stylesheet_directory_uri() . '/js/alto-contraste.js');
+}
+
+add_action( 'wp_head', 'divi_child_enqueue_styles' );
+
 ?>
